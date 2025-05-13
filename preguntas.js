bc58b714a9e532183419345ca0fe00964cf8a0ff
MatiGaete2023 <154522773+MatiGaete2023@users.noreply.github.com>
2025-05-13 15:45:06 -0400

    Add files via upload

---
+++ preguntas.js
@@ -0,0 +1,13 @@
+
+const preguntas_multiples = [
+  {
+    pregunta: "¿Cuál de los siguientes es un elemento esencial del acto jurídico?",
+    opciones: ["Condición", "Objeto", "Plazo", "Modo"],
+    respuesta: 1
+  },
+  {
+    pregunta: "El silencio como manifestación de voluntad en el acto jurídico...",
+    opciones: ["Siempre se considera aceptación", "Nunca produce efectos", "Puede ser relevante si hay norma o costumbre", "Anula el consentimiento"],
+    respuesta: 2
+  }
+];
